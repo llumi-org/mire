4c342d132d07e230e08cec014109cd0e4df6ed49
Colin Wang <colins.wang@hotmail.com>
2026-03-07 12:45:41 +0000

    Add sitemap generation and robots.txt (pointing to sitemap-index.xml)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
 import remarkGfm from 'remark-gfm';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mire.llumi.org',
+  integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkMath, remarkGfm],
     rehypePlugins: [rehypeKatex],
